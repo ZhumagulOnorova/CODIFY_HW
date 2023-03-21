8dea67559804452d415903e1fb097b6be1c2d460
Zhumagul Onorova <zhumagul.onorova.kk@gmail.com>
2023-03-21 09:59:27 +0300

    I add first assigment from JS

---
+++ variables_hw/main.js
@@ -0,0 +1,51 @@
+// Задача 1
+
+
+let name="Джон"
+let admin= name
+console.log(admin)
+
+// 2  задание
+
+let str = 'Привет, Мир!'
+console.log(str)
+
+let str1='Привет,'
+let str2 ='Мир!' 
+console.log(str1+ ' ' +str2)
+
+let name1 = 'Zhumagul !'
+console.log('Привет,' + ' ' + name1)
+
+let age = 20
+console.log('Мне ' + age + ' лет!')
+
+
+// 3  задание
+
+let num = 3
+console.log(num)
+
+let a =10
+let b =2
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+
+let c = 15 
+let d = 2
+let result = c + d
+console.log(result)
+
+let a1 = 10
+let b1 = 2
+let c1 = 5
+console.log(a + b + c)
+
+let a2 = 17
+let b2 = 10
+let c2 = a - b
+let d2 = 7
+let result1 =  c + d
+console.log(result1)  
